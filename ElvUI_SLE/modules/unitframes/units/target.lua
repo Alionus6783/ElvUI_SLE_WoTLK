1294a03aed0af042cf71857704469b3a6936b4c8
Ava <122236644+Alionus6783@users.noreply.github.com>
2023-01-25 17:04:41 -0500

    testing target issues
    base Elv target works, but SLE doesnt, so seeing what i can do with Elv code instead

--- ElvUI_SLE/modules/unitframes/units/target.lua
+++ ElvUI_SLE/modules/unitframes/units/target.lua
@@ -1,40 +1,109 @@
-local SLE, _, E = unpack(select(2, ...))
-local SUF = SLE.UnitFrames
+local E, L, V, P, G = unpack(ElvUI)
+local UF = E:GetModule('UnitFrames')
+local ElvUF = E.oUF
 
-function SUF:Construct_TargetFrame(frame)
-	-- print('Construct_TargetFrame: ', frame:GetName())
-	frame.SL_DeathIndicator = SUF:Construct_DeathIndicator(frame)
-	frame.SL_OfflineIndicator = SUF:Construct_OfflineIndicator(frame)
+local _G = _G
+local tinsert = tinsert
 
-	SUF:Construct_PvPTimerText(frame)
-	SUF:Construct_PvPLevelText(frame)
+function UF:Construct_TargetFrame(frame)
+	frame.Health = UF:Construct_HealthBar(frame, true, true, 'RIGHT')
+	frame.Power = UF:Construct_PowerBar(frame, true, true, 'LEFT')
+	frame.Power.frequentUpdates = true
+	frame.PowerPrediction = UF:Construct_PowerPrediction(frame)
+	frame.Name = UF:Construct_NameText(frame)
+	frame.Portrait3D = UF:Construct_Portrait(frame, 'model')
+	frame.Portrait2D = UF:Construct_Portrait(frame, 'texture')
+	frame.Buffs = UF:Construct_Buffs(frame)
+	frame.Debuffs = UF:Construct_Debuffs(frame)
+	frame.ThreatIndicator = UF:Construct_Threat(frame)
+	frame.Castbar = UF:Construct_Castbar(frame, L["Target Castbar"])
+	frame.Castbar.SafeZone = nil
+	frame.Castbar.LatencyTexture:Hide()
+	frame.RaidTargetIndicator = UF:Construct_RaidIcon(frame)
+	frame.HealthPrediction = UF:Construct_HealComm(frame)
+	frame.AuraHighlight = UF:Construct_AuraHighlight(frame)
+	frame.InfoPanel = UF:Construct_InfoPanel(frame)
+	frame.MouseGlow = UF:Construct_MouseGlow(frame)
+	frame.TargetGlow = UF:Construct_TargetGlow(frame)
+	frame.FocusGlow = UF:Construct_FocusGlow(frame)
+	frame.AuraBars = UF:Construct_AuraBarHeader(frame)
+	frame.PhaseIndicator = UF:Construct_PhaseIcon(frame)
+	frame.ResurrectIndicator = UF:Construct_ResurrectionIcon(frame)
+	frame.RaidRoleFramesAnchor = UF:Construct_RaidRoleFrames(frame)
+	frame.PvPIndicator = UF:Construct_PvPIcon(frame)
+	frame.Fader = UF:Construct_Fader()
+	frame.Cutaway = UF:Construct_Cutaway(frame)
+	frame.CombatIndicator = UF:Construct_CombatIndicator(frame)
 
-	if frame.AuraBars then
-		frame.AuraBars.slBarID = 'aurabar'
-		hooksecurefunc(frame.AuraBars, 'PostUpdateBar', SUF.PostUpdateBar_AuraBars)
-	end
-	if frame.Castbar then
-		frame.Castbar.slBarID = 'castbar'
-	end
-	if frame.Power then
-		frame.Power.slBarID = 'powerbar'
-	end
+	frame.customTexts = {}
+	frame:Point('BOTTOM', E.UIParent, 'BOTTOM', 342, 139)
+	E:CreateMover(frame, frame:GetName()..'Mover', L["Target Frame"], nil, nil, nil, 'ALL,SOLO', nil, 'unitframe,individualUnits,target,generalGroup')
+
+	frame.unitframeType = 'target'
 end
 
-function SUF:Update_TargetFrame(frame)
-	-- print('Update_TargetFrame: ', frame:GetName())
-	if not frame then return end
-	local enableState = E.private.sle.module.shadows.enable and E.db.unitframe.units.target.enable
-	local db = E.db.sle.shadows.unitframes[frame.unitframeType]
+function UF:Update_TargetFrame(frame, db)
+	frame.db = db
+	frame.colors = ElvUF.colors
 
-	frame.SLLEGACY_ENHSHADOW = enableState and db.legacy or false
-	frame.SLHEALTH_ENHSHADOW = enableState and db.health or false
-	frame.SLPOWER_ENHSHADOW = enableState and db.power or false
+	do
+		frame.ORIENTATION = db.orientation --allow this value to change when unitframes position changes on screen?
+		frame.UNIT_WIDTH = db.width
+		frame.UNIT_HEIGHT = db.infoPanel.enable and (db.height + db.infoPanel.height) or db.height
+		frame.USE_POWERBAR = db.power.enable
+		frame.POWERBAR_DETACHED = db.power.detachFromFrame
+		frame.USE_INSET_POWERBAR = not frame.POWERBAR_DETACHED and db.power.width == 'inset' and frame.USE_POWERBAR
+		frame.USE_MINI_POWERBAR = (not frame.POWERBAR_DETACHED and db.power.width == 'spaced' and frame.USE_POWERBAR)
+		frame.USE_POWERBAR_OFFSET = (db.power.width == 'offset' and db.power.offset ~= 0) and frame.USE_POWERBAR and not frame.POWERBAR_DETACHED
+		frame.POWERBAR_OFFSET = frame.USE_POWERBAR_OFFSET and db.power.offset or 0
+		frame.POWERBAR_HEIGHT = not frame.USE_POWERBAR and 0 or db.power.height
+		frame.POWERBAR_WIDTH = frame.USE_MINI_POWERBAR and (frame.UNIT_WIDTH - (UF.BORDER*2))*0.5 or (frame.POWERBAR_DETACHED and db.power.detachedWidth or (frame.UNIT_WIDTH - ((UF.BORDER+UF.SPACING)*2)))
+		frame.USE_PORTRAIT = db.portrait and db.portrait.enable
+		frame.USE_PORTRAIT_OVERLAY = frame.USE_PORTRAIT and (db.portrait.overlay or frame.ORIENTATION == 'MIDDLE')
+		frame.PORTRAIT_WIDTH = (frame.USE_PORTRAIT_OVERLAY or not frame.USE_PORTRAIT) and 0 or db.portrait.width
+		frame.USE_INFO_PANEL = not frame.USE_MINI_POWERBAR and not frame.USE_POWERBAR_OFFSET and db.infoPanel.enable
+		frame.INFO_PANEL_HEIGHT = frame.USE_INFO_PANEL and db.infoPanel.height or 0
+		frame.BOTTOM_OFFSET = UF:GetHealthBottomOffset(frame)
+	end
 
-	SUF:Configure_Health(frame)
-	SUF:Configure_Power(frame)
-	SUF:Configure_DeathIndicator(frame)
-	SUF:Configure_OfflineIndicator(frame)
-	SUF:Configure_PvPTimerText(frame)
-	SUF:Configure_PvPLevelText(frame)
+	if db.strataAndLevel and db.strataAndLevel.useCustomStrata then
+		frame:SetFrameStrata(db.strataAndLevel.frameStrata)
+	end
+
+	if db.strataAndLevel and db.strataAndLevel.useCustomLevel then
+		frame:SetFrameLevel(db.strataAndLevel.frameLevel)
+	end
+
+	frame:Size(frame.UNIT_WIDTH, frame.UNIT_HEIGHT)
+	_G[frame:GetName()..'Mover']:Size(frame:GetSize())
+
+	UF:Configure_InfoPanel(frame)
+	UF:Configure_HealthBar(frame)
+	UF:UpdateNameSettings(frame)
+	UF:Configure_Power(frame)
+	UF:Configure_PowerPrediction(frame)
+	UF:Configure_Portrait(frame)
+	UF:Configure_Threat(frame)
+	UF:EnableDisable_Auras(frame)
+	UF:Configure_AllAuras(frame)
+	UF:Configure_RaidRoleIcons(frame)
+	UF:Configure_AuraHighlight(frame)
+	UF:Configure_HealComm(frame)
+	UF:Configure_ResurrectionIcon(frame)
+	UF:Configure_RaidIcon(frame)
+	UF:Configure_AuraBars(frame)
+	UF:Configure_PhaseIcon(frame)
+	UF:Configure_PVPIcon(frame)
+	UF:Configure_Cutaway(frame)
+	UF:Configure_CustomTexts(frame)
+	UF:Configure_CombatIndicator(frame)
+	UF:Configure_Castbar(frame)
+	UF:Configure_Fader(frame)
+
+	UF:HandleRegisterClicks(frame)
+
+	E:SetMoverSnapOffset(frame:GetName()..'Mover', -(12 + db.castbar.height))
+	frame:UpdateAllElements('ElvUI_UpdateAllElements')
 end
+
+tinsert(UF.unitstoload, 'target')
